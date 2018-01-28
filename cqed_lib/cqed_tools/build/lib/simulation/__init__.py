902c899a4ce053ab843d6142d8c04e2f015ba5e4
Paul Brookes <pbrookes@pang.phys.ucl.ac.uk>
2018-01-28 18:42:57 +0000

    Removed build files from being tracked.

--- cqed_lib/cqed_tools/build/lib/simulation/__init__.py
+++
@@ -1,4 +0,0 @@
-from legion_tools import *
-from slowdown_sim import *
-from spectrum_calc import *
-from ss_collation import *
--- run_queue.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if __name__ == '__main__':
-    queue_dir = 'queue'
-    for filename in os.listdir(queue_dir):
-        os.system('python pang_spectrum.py ../Scratch/queue -r True -s ' + queue_dir + '/' +filename)
