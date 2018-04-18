b64e8ea0b0a4fb757c29acc78a17b83ca330623e
Paul Brookes <paul.s.brookes@gmail.com>
2018-04-18 11:39:25 +0300

    Debug.

--- cqed_lib/cqed_tools/simulation/liouvillian_sim_alt.py
+++ cqed_lib/cqed_tools/simulation/liouvillian_sim_alt.py
@@ -1,12 +1,16 @@
 from .legion_tools import *
 import scipy.sparse.linalg as lin
 from qutip.cy.spconvert import dense2D_to_fastcsr_fmode
+import h5py
 
 def hdf_append(path,data,key):
     if os.path.exists(path):
-        loaded = pd.read_hdf(path,key=key)
+        f = h5py.File(path, 'r')
+        if key in f.keys():
+            f.close()
+            loaded = pd.read_hdf(path,key=key)
     else:
-        loaded= pd.DataFrame()
+        loaded = pd.DataFrame()
     combined = loaded.append(data)
     combined.to_hdf(path,key=key,mode='a')
 
