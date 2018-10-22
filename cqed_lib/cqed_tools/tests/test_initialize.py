34dac305c96c5698388bec7edb0c82e74616bc52
Paul Brookes <pbrookes@pang.phys.ucl.ac.uk>
2018-10-22 22:10:02 +0100

    Started adding tests.

---
+++ cqed_lib/cqed_tools/tests/test_initialize.py
@@ -0,0 +1,9 @@
+from numpy.testing import assert_, run_module_suite
+
+
+def test_print():
+    print('Testing print.')
+
+
+if __name__ == "__main__":
+    run_module_suite()
---
+++ cqed_lib/cqed_tools/tests/test_mf.py
@@ -0,0 +1,26 @@
+from numpy.testing import assert_, assert_equal, run_module_suite
+from cqed_tools.mf.hamiltonian import *
+import pandas as pd
+
+
+def test_collapse_operators_mf():
+
+    params = pd.Series([4, 1.0, 2.0, 3.0], index=['t_levels', 'gamma', 'gamma_phi', 'n_t'], dtype=object)
+    c_ops = collapse_operators_mf(params)
+    assert_equal(c_ops[0], destroy(params.t_levels) * np.sqrt(params.gamma * (params.n_t + 1)))
+    assert_equal(c_ops[1], create(params.t_levels) * np.sqrt(params.gamma * params.n_t))
+    assert_equal(len(c_ops), 3)
+
+    params = pd.Series([4, 0.0, 2.0, 3.0], index=['t_levels', 'gamma', 'gamma_phi', 'n_t'], dtype=object)
+    c_ops = collapse_operators_mf(params)
+    assert_equal(len(c_ops), 1)
+
+    params = pd.Series([4, 2.0, 0.0, 3.0], index=['t_levels', 'gamma', 'gamma_phi', 'n_t'], dtype=object)
+    c_ops = collapse_operators_mf(params)
+    assert_equal(c_ops[0], destroy(params.t_levels) * np.sqrt(params.gamma * (params.n_t + 1)))
+    assert_equal(c_ops[1], create(params.t_levels) * np.sqrt(params.gamma * params.n_t))
+    assert_equal(len(c_ops), 2)
+
+
+if __name__ == "__main__":
+    run_module_suite()
