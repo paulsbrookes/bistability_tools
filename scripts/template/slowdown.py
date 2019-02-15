6662c3a88dce6af537e9de6ec9bf066fef5c07d1
Paul Brookes <paul.s.brookes@gmail.com>
2019-02-15 22:34:40 +0000

    Make mf initialization optional.

--- scripts/template/slowdown.py
+++ scripts/template/slowdown.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     parser.add_argument('output_directory', type=str)
     parser.add_argument('-b', '--bistable_initial', default=True, type=str2bool)
     parser.add_argument('-t', '--transformation', default=False, type=str2bool)
+    parser.add_argument('-mf', '--mf_init', default=False, type=str2bool)
+    parser.add_argument('-g', '--g', default=np.sqrt(2), type=float)
     args = parser.parse_args()
     output_directory = args.output_directory
     bistable_initial = args.bistable_initial
@@ -17,4 +19,7 @@ if __name__ == '__main__':
     kwargs['output_directory'] = output_directory
     kwargs['bistable_initial'] = bistable_initial
     kwargs['transformation'] = transformation
+    kwargs['g'] = args.g
+    kwargs['mf_init'] = args.mf_init
+    print('In slowdown.py we have bistable_initial = ' + str(bistable_initial))
     mpi_allocator(slowdown_sim, args, kwargs)
