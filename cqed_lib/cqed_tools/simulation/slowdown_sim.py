5cc4c45fb2af74026d386b2aa7da45aeb23ebfd0
Paul Brookes <ucappsb@login01.grace.ucl.ac.uk>
2019-02-15 20:25:45 +0000

    Increasing nsteps in slowdown sim options.

--- cqed_lib/cqed_tools/simulation/slowdown_sim.py
+++ cqed_lib/cqed_tools/simulation/slowdown_sim.py
@@ -51,7 +51,7 @@ def slowdown_sim(job_index, output_directory='./results', bistable_initial=True,
     print('The working directory for the current job index is ' + str(directory))
     sys_params.to_csv('settings.csv')
 
-    options = Options(nsteps=2000000000)
+    options = Options(nsteps=200000000000)
 
     if os.path.exists('./state_checkpoint.qu'):
         print('Loading state checkpoint for job_index = '+str(sys_params.job_index))
